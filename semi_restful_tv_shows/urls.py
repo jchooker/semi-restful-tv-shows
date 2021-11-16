96e1b4c6a0f9bbf3d94d0a6a95ca37d11c68878e
Joseph Hooker <jcolehooker@gmail.com>
2021-11-15 23:18:41 -0600

    Early HTML/CSS/Django setup

---
+++ semi_restful_tv_shows/urls.py
@@ -0,0 +1,8 @@
+
+# from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+    path('', include('semi_restful_tv_shows.urls')),
+]
