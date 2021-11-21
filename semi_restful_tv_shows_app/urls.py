6357baaf2d6fba538ea059b50c04eeb58923a59d
Joseph Hooker <jcolehooker@gmail.com>
2021-11-20 22:40:30 -0600

    Continuing to build html/css; added one Show entry to database; expanding urls.py and views.py; models.py should be complete

--- semi_restful_tv_shows_app/urls.py
+++ semi_restful_tv_shows_app/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.allShows),
+    path('', views.all_shows),
+    path('shows/new', views.show_form),
+    path('shows/create', views.add_info),
+    path('shows/<int:id>', views.show_info),
+    path('shows/<int:id>/edit', views.edit_show),
+    path('shows/<int:id>/update', views.update_show),
+    path('shows/<int:id>/destroy', views.delete_show)
 ]
--- semi_restful_tv_shows_app/views.py
+++ semi_restful_tv_shows_app/views.py
@@ -2,8 +2,32 @@ from django.shortcuts import render
 from .models import *
 
 # Create your views here.
-def allShows(request):
+def all_shows(request):
     context = {
         'shows':Show.objects.all(),
     }
-    return render(request, "all-shows.html", context)
+    return render(request, "shows.html", context)
+
+def show_form(request):
+    pass
+
+def add_info(request):
+    pass
+
+def show_info(request, id):
+    context = {
+        'show':Show.objects.get(id=id)
+    }
+    return render(request, "view-show.html", context)
+
+def edit_show(request, id):
+    context = {
+        'show':Show.objects.get(id=id),
+    }
+    return render(request, "edit-show.html", context)
+
+def update_show(request):
+    pass
+
+def delete_show(request):
+    pass
